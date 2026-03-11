8ec880314bba971ceeb9bc8198e788226b35fa00
Your Name <Roni>
2026-03-11 21:55:20 +0100

    detailpage, dailychallenges, tutorial - Mobile

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,17 +1,37 @@
-let parentballs = document.querySelector(".tutorial-circles")
-let balls = parentballs.querySelectorAll("div");
+const titles = ["Welcome to Un(Comfy)!", "Voltooi dagelijkse uitdagingen","Bouw je streaks op", "Verdien ComfyPoints", "Klaar om te starten"];
+const text = ["Stap uit je comfortzone en groei met dagelijkse uitdagingen die zijn ontworpen om je te helpen de beste versie van jezelf te worden.", "Ga elke dag leuke en zinvolle uitdagingen aan. Van complimenten geven tot ochtendmeditaties, elke taak helpt je groeien.",
+        "Blijf consequent en zie je reeks groeien! Hoe meer dagen je volhoudt, hoe sterker je gewoontes worden.", "Voltooi uitdagingen om ComfyPoints te verdienen! Gebruik ze om speciale functies te ontgrendelen en je voortgang bij te houden.",
+        "Jouw reis begint nu. Laten we elke dag benutten en samen de onbekende wereld verkennen!"];
+
+
+let balls = document.querySelectorAll(".tutorial-circles div");
+
+let returnButton = document.querySelector(".return-button");
+
+let titleTutorial = document.querySelector(".title-tutorial");
+let textTutorial = document.querySelector(".text-tutorial");
+
+let counter = 0;
+
+balls[counter].classList.add("active");
+
+
+console.log(balls.length);
+
 
 let continueButton = document.querySelector(".continue-button")
 
-for (let index = 0; index < balls.length; index++) {
+        titleTutorial.textContent = titles[counter];
+        textTutorial.textContent = text[counter];
 
         continueButton.addEventListener("click",(e) => {
         e.preventDefault();
-        balls[1].classList.add("active");
-
-        console.log(`${continueButton} button pressed`);
-    } )
-
-
-    
-}
+        counter++;
+        balls[counter].classList.add("active");
+        balls[counter - 1].classList.remove("active");
+        titleTutorial.textContent = titles[counter];
+        textTutorial.textContent = text[counter];
+        
+        console.log(`button pressed`);
+        returnButton.style.display = "flex";        
+        });
